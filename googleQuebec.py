f7d4c7fbe0c3b85bd8aef0397ea051be44c72991
Jean-Hugues Roy <jh@jhroy.ca>
2021-06-02 15:50:30 -0400

    ajout fichier termes+villes

---
+++ googleQuebec.py
@@ -0,0 +1,974 @@
+termes = [
+	[
+		'accident',
+		'décès OR mort',
+		'incendie',
+		'police OR Sûreté',
+		'Cégep',
+		'"Centre de services scolaire" OR "Commission scolaire"',
+		'CISSS OR CIUSSS',
+		'maire OR mairesse',
+		'candidat',
+		'préfet',
+		'travaux routiers',
+		'autobus OR tramway'
+	],
+	[
+		'"premier ministre"',
+		'élection OR campagne',
+		'député',
+		'racisme',
+		'"arme à feu" OR "arme à feu"',
+		'santé publique',
+		'covid OR COVID',
+		'immigration',
+		'climat',
+		'météo',
+		'construction'
+	],
+	[
+		'Chine',
+		'Biden OR Trump',
+		'déficit'
+	]
+]
+
+villes = [
+ {
+   "code": 48028,
+   "ville": "Acton Vale ",
+   "pop": 7743,
+   "latitude": 45.64778,
+   "longitude": -72.56472,
+   "codeMRC": 48,
+   "MRC": "Acton"
+ },
+ {
+   "code": 93042,
+   "ville": "Alma ",
+   "pop": 30791,
+   "latitude": 48.54972,
+   "longitude": -71.65333,
+   "codeMRC": 93,
+   "MRC": "Lac-Saint-Jean-Est"
+ },
+ {
+   "code": 88055,
+   "ville": "Amos ",
+   "pop": 12734,
+   "latitude": 48.56472,
+   "longitude": -78.1125,
+   "codeMRC": 88,
+   "MRC": "Abitibi"
+ },
+ {
+   "code": 7047,
+   "ville": "Amqui ",
+   "pop": 6082,
+   "latitude": 48.46194,
+   "longitude": -67.43222,
+   "codeMRC": 7,
+   "MRC": "La Matapédia"
+ },
+ {
+   "code": 96020,
+   "ville": "Baie-Comeau ",
+   "pop": 21061,
+   "latitude": 49.22083,
+   "longitude": -68.14917,
+   "codeMRC": 96,
+   "MRC": "Manicouagan"
+ },
+ {
+   "code": 16013,
+   "ville": "Baie-Saint-Paul ",
+   "pop": 7168,
+   "latitude": 47.44056,
+   "longitude": -70.50944,
+   "codeMRC": 16,
+   "MRC": "Charlevoix"
+ },
+ {
+   "code": 27028,
+   "ville": "Beauceville ",
+   "pop": 6313,
+   "latitude": 46.21472,
+   "longitude": -70.77583,
+   "codeMRC": 27,
+   "MRC": "Robert-Cliche"
+ },
+ {
+   "code": 38010,
+   "ville": "Bécancour ",
+   "pop": 13545,
+   "latitude": 46.34833,
+   "longitude": -72.435,
+   "codeMRC": 38,
+   "MRC": "Bécancour"
+ },
+ {
+   "code": 73015,
+   "ville": "Blainville ",
+   "pop": 60695,
+   "latitude": 45.67944,
+   "longitude": -73.87389,
+   "codeMRC": 73,
+   "MRC": "Thérèse-De Blainville"
+ },
+ {
+   "code": 98005,
+   "ville": "Blanc-Sablon ",
+   "pop": 1085,
+   "latitude": 51.4175,
+   "longitude": -57.18528,
+   "codeMRC": 982,
+   "MRC": "Le Golfe-du-Saint-Laurent"
+ },
+ {
+   "code": 21045,
+   "ville": "Boischatel ",
+   "pop": 8148,
+   "latitude": 46.89694,
+   "longitude": -71.14,
+   "codeMRC": 21,
+   "MRC": "La Côte-de-Beaupré"
+ },
+ {
+   "code": 6013,
+   "ville": "Carleton-sur-Mer ",
+   "pop": 3985,
+   "latitude": 48.10167,
+   "longitude": -66.12194,
+   "codeMRC": 6,
+   "MRC": "Avignon"
+ },
+ {
+   "code": 57005,
+   "ville": "Chambly ",
+   "pop": 31360,
+   "latitude": 45.45,
+   "longitude": -73.29111,
+   "codeMRC": 57,
+   "MRC": "La Vallée-du-Richelieu"
+ },
+ {
+   "code": 2028,
+   "ville": "Chandler ",
+   "pop": 7400,
+   "latitude": 48.34861,
+   "longitude": -64.67833,
+   "codeMRC": 2,
+   "MRC": "Le Rocher-Percé"
+ },
+ {
+   "code": 67050,
+   "ville": "Châteauguay ",
+   "pop": 49927,
+   "latitude": 45.36083,
+   "longitude": -73.75056,
+   "codeMRC": 67,
+   "MRC": "Roussillon"
+ },
+ {
+   "code": 99025,
+   "ville": "Chibougamau ",
+   "pop": 7493,
+   "latitude": 49.91333,
+   "longitude": -74.36861,
+   "codeMRC": 991,
+   "MRC": "Jamésie"
+ },
+ {
+   "code": 99814,
+   "ville": "Chisasibi ",
+   "pop": 5203,
+   "latitude": 53.65528,
+   "longitude": -78.79778,
+   "codeMRC": 993,
+   "MRC": "Eeyou Istchee"
+ },
+ {
+   "code": 44037,
+   "ville": "Coaticook ",
+   "pop": 8906,
+   "latitude": 45.13444,
+   "longitude": -71.805,
+   "codeMRC": 44,
+   "MRC": "Coaticook"
+ },
+ {
+   "code": 41038,
+   "ville": "Cookshire-Eaton ",
+   "pop": 5431,
+   "latitude": 45.41306,
+   "longitude": -71.62611,
+   "codeMRC": 41,
+   "MRC": "Le Haut-Saint-François"
+ },
+ {
+   "code": 46080,
+   "ville": "Cowansville ",
+   "pop": 14695,
+   "latitude": 45.20472,
+   "longitude": -72.74111,
+   "codeMRC": 46,
+   "MRC": "Brome-Missisquoi"
+ },
+ {
+   "code": 92022,
+   "ville": "Dolbeau-Mistassini ",
+   "pop": 14072,
+   "latitude": 48.87639,
+   "longitude": -72.22583,
+   "codeMRC": 92,
+   "MRC": "Maria-Chapdelaine"
+ },
+ {
+   "code": 49058,
+   "ville": "Drummondville ",
+   "pop": 78376,
+   "latitude": 45.88056,
+   "longitude": -72.48417,
+   "codeMRC": 49,
+   "MRC": "Drummond"
+ },
+ {
+   "code": 97035,
+   "ville": "Fermont ",
+   "pop": 2447,
+   "latitude": 52.7975,
+   "longitude": -67.09028,
+   "codeMRC": 972,
+   "MRC": "Caniapiscau"
+ },
+ {
+   "code": 95045,
+   "ville": "Forestville ",
+   "pop": 2924,
+   "latitude": 48.73833,
+   "longitude": -69.07694,
+   "codeMRC": 95,
+   "MRC": "La Haute-Côte-Nord"
+ },
+ {
+   "code": 3005,
+   "ville": "Gaspé ",
+   "pop": 15024,
+   "latitude": 48.82472,
+   "longitude": -64.47611,
+   "codeMRC": 3,
+   "MRC": "La Côte-de-Gaspé"
+ },
+ {
+   "code": 81017,
+   "ville": "Gatineau ",
+   "pop": 287868,
+   "latitude": 45.42778,
+   "longitude": -75.71111,
+   "codeMRC": 81,
+   "MRC": "Gatineau"
+ },
+ {
+   "code": 47017,
+   "ville": "Granby ",
+   "pop": 68540,
+   "latitude": 45.39889,
+   "longitude": -72.72111,
+   "codeMRC": 47,
+   "MRC": "La Haute-Yamaska"
+ },
+ {
+   "code": 98040,
+   "ville": "Havre-Saint-Pierre ",
+   "pop": 3371,
+   "latitude": 50.245,
+   "longitude": -63.59694,
+   "codeMRC": 981,
+   "MRC": "Minganie"
+ },
+ {
+   "code": 61025,
+   "ville": "Joliette ",
+   "pop": 21096,
+   "latitude": 46.02389,
+   "longitude": -73.44278,
+   "codeMRC": 61,
+   "MRC": "Joliette"
+ },
+ {
+   "code": 99095,
+   "ville": "Kuujjuaq ",
+   "pop": 2848,
+   "latitude": 58.1075,
+   "longitude": -68.40361,
+   "codeMRC": 992,
+   "MRC": "Administration régionale Kativik"
+ },
+ {
+   "code": 17078,
+   "ville": "L'Islet ",
+   "pop": 3803,
+   "latitude": 47.10361,
+   "longitude": -70.35194,
+   "codeMRC": 17,
+   "MRC": "L'Islet"
+ },
+ {
+   "code": 15013,
+   "ville": "La Malbaie ",
+   "pop": 8141,
+   "latitude": 47.65222,
+   "longitude": -70.14889,
+   "codeMRC": 15,
+   "MRC": "Charlevoix-Est"
+ },
+ {
+   "code": 14085,
+   "ville": "La Pocatière ",
+   "pop": 4051,
+   "latitude": 47.37028,
+   "longitude": -70.03278,
+   "codeMRC": 14,
+   "MRC": "Kamouraska"
+ },
+ {
+   "code": 87090,
+   "ville": "La Sarre ",
+   "pop": 7305,
+   "latitude": 48.80694,
+   "longitude": -79.20472,
+   "codeMRC": 87,
+   "MRC": "Abitibi-Ouest"
+ },
+ {
+   "code": 90012,
+   "ville": "La Tuque ",
+   "pop": 10912,
+   "latitude": 47.44083,
+   "longitude": -72.78139,
+   "codeMRC": 90,
+   "MRC": "La Tuque"
+ },
+ {
+   "code": 28053,
+   "ville": "Lac-Etchemin ",
+   "pop": 3878,
+   "latitude": 46.39639,
+   "longitude": -70.50444,
+   "codeMRC": 28,
+   "MRC": "Les Etchemins"
+ },
+ {
+   "code": 30030,
+   "ville": "Lac-Mégantic ",
+   "pop": 5569,
+   "latitude": 45.57361,
+   "longitude": -70.88222,
+   "codeMRC": 30,
+   "MRC": "Le Granit"
+ },
+ {
+   "code": 76020,
+   "ville": "Lachute ",
+   "pop": 13393,
+   "latitude": 45.65556,
+   "longitude": -74.33944,
+   "codeMRC": 76,
+   "MRC": "Argenteuil"
+ },
+ {
+   "code": 65005,
+   "ville": "Laval ",
+   "pop": 438973,
+   "latitude": 45.55167,
+   "longitude": -73.74528,
+   "codeMRC": 65,
+   "MRC": "Laval"
+ },
+ {
+   "code": 52007,
+   "ville": "Lavaltrie ",
+   "pop": 14195,
+   "latitude": 45.88444,
+   "longitude": -73.27694,
+   "codeMRC": 52,
+   "MRC": "D'Autray"
+ },
+ {
+   "code": 1023,
+   "ville": "Les Îles-de-la-Madeleine ",
+   "pop": 12168,
+   "latitude": 47.37611,
+   "longitude": -61.86806,
+   "codeMRC": 1,
+   "MRC": "Communauté maritime des Îles-de-la-Madeleine"
+ },
+ {
+   "code": 25213,
+   "ville": "Lévis ",
+   "pop": 147673,
+   "latitude": 46.75806,
+   "longitude": -71.24083,
+   "codeMRC": 251,
+   "MRC": "Lévis"
+ },
+ {
+   "code": 58227,
+   "ville": "Longueuil ",
+   "pop": 249997,
+   "latitude": 45.51389,
+   "longitude": -73.43139,
+   "codeMRC": 58,
+   "MRC": "Longueuil"
+ },
+ {
+   "code": 51015,
+   "ville": "Louiseville ",
+   "pop": 7183,
+   "latitude": 46.25556,
+   "longitude": -72.9425,
+   "codeMRC": 51,
+   "MRC": "Maskinongé"
+ },
+ {
+   "code": 45072,
+   "ville": "Magog ",
+   "pop": 27280,
+   "latitude": 45.265,
+   "longitude": -72.14667,
+   "codeMRC": 45,
+   "MRC": "Memphrémagog"
+ },
+ {
+   "code": 83065,
+   "ville": "Maniwaki ",
+   "pop": 3828,
+   "latitude": 46.37444,
+   "longitude": -75.98194,
+   "codeMRC": 83,
+   "MRC": "La Vallée-de-la-Gatineau"
+ },
+ {
+   "code": 84065,
+   "ville": "Mansfield-et-Pontefract ",
+   "pop": 2341,
+   "latitude": 45.86111,
+   "longitude": -76.73917,
+   "codeMRC": 84,
+   "MRC": "Pontiac"
+ },
+ {
+   "code": 55048,
+   "ville": "Marieville ",
+   "pop": 11159,
+   "latitude": 45.43139,
+   "longitude": -73.16361,
+   "codeMRC": 55,
+   "MRC": "Rouville"
+ },
+ {
+   "code": 8053,
+   "ville": "Matane ",
+   "pop": 14051,
+   "latitude": 48.84333,
+   "longitude": -67.53056,
+   "codeMRC": 8,
+   "MRC": "La Matanie"
+ },
+ {
+   "code": 74005,
+   "ville": "Mirabel ",
+   "pop": 57500,
+   "latitude": 45.66417,
+   "longitude": -74.00194,
+   "codeMRC": 74,
+   "MRC": "Mirabel"
+ },
+ {
+   "code": 9077,
+   "ville": "Mont-Joli ",
+   "pop": 6218,
+   "latitude": 48.58528,
+   "longitude": -68.18917,
+   "codeMRC": 9,
+   "MRC": "La Mitis"
+ },
+ {
+   "code": 79088,
+   "ville": "Mont-Laurier ",
+   "pop": 14160,
+   "latitude": 46.54611,
+   "longitude": -75.50306,
+   "codeMRC": 79,
+   "MRC": "Antoine-Labelle"
+ },
+ {
+   "code": 18050,
+   "ville": "Montmagny ",
+   "pop": 11088,
+   "latitude": 46.98139,
+   "longitude": -70.5575,
+   "codeMRC": 18,
+   "MRC": "Montmagny"
+ },
+ {
+   "code": 5070,
+   "ville": "New Richmond ",
+   "pop": 3718,
+   "latitude": 48.16222,
+   "longitude": -65.86139,
+   "codeMRC": 5,
+   "MRC": "Bonaventure"
+ },
+ {
+   "code": 50072,
+   "ville": "Nicolet ",
+   "pop": 8415,
+   "latitude": 46.22639,
+   "longitude": -72.61611,
+   "codeMRC": 50,
+   "MRC": "Nicolet-Yamaska"
+ },
+ {
+   "code": 37235,
+   "ville": "Notre-Dame-du-Mont-Carmel ",
+   "pop": 5953,
+   "latitude": 46.47278,
+   "longitude": -72.67556,
+   "codeMRC": 372,
+   "MRC": "Les Chenaux"
+ },
+ {
+   "code": 69037,
+   "ville": "Ormstown ",
+   "pop": 3676,
+   "latitude": 45.12111,
+   "longitude": -73.99083,
+   "codeMRC": 69,
+   "MRC": "Le Haut-Saint-Laurent"
+ },
+ {
+   "code": 32040,
+   "ville": "Plessisville ",
+   "pop": 6596,
+   "latitude": 46.21889,
+   "longitude": -71.77194,
+   "codeMRC": 32,
+   "MRC": "L'Érable"
+ },
+ {
+   "code": 23027,
+   "ville": "Québec ",
+   "pop": 546424,
+   "latitude": 46.81389,
+   "longitude": -71.20806,
+   "codeMRC": 235,
+   "MRC": "Communauté métropolitaine de Québec"
+ },
+ {
+   "code": 62037,
+   "ville": "Rawdon ",
+   "pop": 11421,
+   "latitude": 46.04222,
+   "longitude": -73.70722,
+   "codeMRC": 62,
+   "MRC": "Matawinie"
+ },
+ {
+   "code": 60013,
+   "ville": "Repentigny ",
+   "pop": 85676,
+   "latitude": 45.74194,
+   "longitude": -73.45139,
+   "codeMRC": 60,
+   "MRC": "L'Assomption"
+ },
+ {
+   "code": 10043,
+   "ville": "Rimouski ",
+   "pop": 49419,
+   "latitude": 48.44917,
+   "longitude": -68.525,
+   "codeMRC": 10,
+   "MRC": "Rimouski-Neigette"
+ },
+ {
+   "code": 12072,
+   "ville": "Rivière-du-Loup ",
+   "pop": 20055,
+   "latitude": 47.83611,
+   "longitude": -69.53667,
+   "codeMRC": 12,
+   "MRC": "Rivière-du-Loup"
+ },
+ {
+   "code": 86042,
+   "ville": "Rouyn-Noranda ",
+   "pop": 43055,
+   "latitude": 48.2375,
+   "longitude": -79.01722,
+   "codeMRC": 86,
+   "MRC": "Rouyn-Noranda"
+ },
+ {
+   "code": 94068,
+   "ville": "Saguenay ",
+   "pop": 146797,
+   "latitude": 48.42778,
+   "longitude": -71.06278,
+   "codeMRC": 941,
+   "MRC": "Saguenay"
+ },
+ {
+   "code": 80027,
+   "ville": "Saint-André-Avellin ",
+   "pop": 3776,
+   "latitude": 45.725,
+   "longitude": -75.05417,
+   "codeMRC": 80,
+   "MRC": "Papineau"
+ },
+ {
+   "code": 33090,
+   "ville": "Saint-Apollinaire ",
+   "pop": 7086,
+   "latitude": 46.61667,
+   "longitude": -71.51056,
+   "codeMRC": 33,
+   "MRC": "Lotbinière"
+ },
+ {
+   "code": 72005,
+   "ville": "Saint-Eustache ",
+   "pop": 45093,
+   "latitude": 45.5575,
+   "longitude": -73.88833,
+   "codeMRC": 72,
+   "MRC": "Deux-Montagnes"
+ },
+ {
+   "code": 91042,
+   "ville": "Saint-Félicien ",
+   "pop": 10286,
+   "latitude": 48.65278,
+   "longitude": -72.44667,
+   "codeMRC": 91,
+   "MRC": "Le Domaine-du-Roy"
+ },
+ {
+   "code": 29073,
+   "ville": "Saint-Georges ",
+   "pop": 33370,
+   "latitude": 46.12278,
+   "longitude": -70.67139,
+   "codeMRC": 29,
+   "MRC": "Beauce-Sartigan"
+ },
+ {
+   "code": 19068,
+   "ville": "Saint-Henri ",
+   "pop": 5744,
+   "latitude": 46.6925,
+   "longitude": -71.0675,
+   "codeMRC": 19,
+   "MRC": "Bellechasse"
+ },
+ {
+   "code": 94240,
+   "ville": "Saint-Honoré ",
+   "pop": 6029,
+   "latitude": 48.53472,
+   "longitude": -71.08639,
+   "codeMRC": 942,
+   "MRC": "Le Fjord-du-Saguenay"
+ },
+ {
+   "code": 54048,
+   "ville": "Saint-Hyacinthe ",
+   "pop": 56810,
+   "latitude": 45.62611,
+   "longitude": -72.94639,
+   "codeMRC": 54,
+   "MRC": "Les Maskoutains"
+ },
+ {
+   "code": 56083,
+   "ville": "Saint-Jean-sur-Richelieu ",
+   "pop": 97711,
+   "latitude": 45.30556,
+   "longitude": -73.25361,
+   "codeMRC": 56,
+   "MRC": "Le Haut-Richelieu"
+ },
+ {
+   "code": 75017,
+   "ville": "Saint-Jérôme ",
+   "pop": 78747,
+   "latitude": 45.77528,
+   "longitude": -74.00361,
+   "codeMRC": 75,
+   "MRC": "La Rivière-du-Nord"
+ },
+ {
+   "code": 63048,
+   "ville": "Saint-Lin--Laurentides ",
+   "pop": 22589,
+   "latitude": 45.85139,
+   "longitude": -73.75583,
+   "codeMRC": 63,
+   "MRC": "Montcalm"
+ },
+ {
+   "code": 20025,
+   "ville": "Saint-Pierre-de-l'Île-d'Orléans ",
+   "pop": 2004,
+   "latitude": 46.88833,
+   "longitude": -71.07,
+   "codeMRC": 20,
+   "MRC": "L'Île-d'Orléans"
+ },
+ {
+   "code": 34128,
+   "ville": "Saint-Raymond ",
+   "pop": 10761,
+   "latitude": 46.89139,
+   "longitude": -71.8325,
+   "codeMRC": 34,
+   "MRC": "Portneuf"
+ },
+ {
+   "code": 68055,
+   "ville": "Saint-Rémi ",
+   "pop": 8836,
+   "latitude": 45.25917,
+   "longitude": -73.61417,
+   "codeMRC": 68,
+   "MRC": "Les Jardins-de-Napierville"
+ },
+ {
+   "code": 35027,
+   "ville": "Saint-Tite ",
+   "pop": 3693,
+   "latitude": 46.72694,
+   "longitude": -72.56472,
+   "codeMRC": 35,
+   "MRC": "Mékinac"
+ },
+ {
+   "code": 77022,
+   "ville": "Sainte-Adèle ",
+   "pop": 13516,
+   "latitude": 45.95556,
+   "longitude": -74.13944,
+   "codeMRC": 77,
+   "MRC": "Les Pays-d'en-Haut"
+ },
+ {
+   "code": 78032,
+   "ville": "Sainte-Agathe-des-Monts ",
+   "pop": 10786,
+   "latitude": 46.04806,
+   "longitude": -74.28361,
+   "codeMRC": 78,
+   "MRC": "Les Laurentides"
+ },
+ {
+   "code": 4037,
+   "ville": "Sainte-Anne-des-Monts ",
+   "pop": 6277,
+   "latitude": 49.12972,
+   "longitude": -66.4875,
+   "codeMRC": 4,
+   "MRC": "La Haute-Gaspésie"
+ },
+ {
+   "code": 59010,
+   "ville": "Sainte-Julie ",
+   "pop": 30095,
+   "latitude": 45.58778,
+   "longitude": -73.32611,
+   "codeMRC": 59,
+   "MRC": "Marguerite-D'Youville"
+ },
+ {
+   "code": 26030,
+   "ville": "Sainte-Marie ",
+   "pop": 13583,
+   "latitude": 46.43833,
+   "longitude": -71.02139,
+   "codeMRC": 26,
+   "MRC": "La Nouvelle-Beauce"
+ },
+ {
+   "code": 70052,
+   "ville": "Salaberry-de-Valleyfield ",
+   "pop": 42306,
+   "latitude": 45.25528,
+   "longitude": -74.13167,
+   "codeMRC": 70,
+   "MRC": "Beauharnois-Salaberry"
+ },
+ {
+   "code": 97007,
+   "ville": "Sept-Îles ",
+   "pop": 25063,
+   "latitude": 50.20972,
+   "longitude": -66.38528,
+   "codeMRC": 971,
+   "MRC": "Sept-Rivières"
+ },
+ {
+   "code": 36033,
+   "ville": "Shawinigan ",
+   "pop": 49382,
+   "latitude": 46.54111,
+   "longitude": -72.74528,
+   "codeMRC": 36,
+   "MRC": "Shawinigan"
+ },
+ {
+   "code": 43027,
+   "ville": "Sherbrooke ",
+   "pop": 168518,
+   "latitude": 45.40417,
+   "longitude": -71.89306,
+   "codeMRC": 43,
+   "MRC": "Sherbrooke"
+ },
+ {
+   "code": 53052,
+   "ville": "Sorel-Tracy ",
+   "pop": 34890,
+   "latitude": 46.04361,
+   "longitude": -73.11333,
+   "codeMRC": 53,
+   "MRC": "Pierre-De Saurel"
+ },
+ {
+   "code": 22035,
+   "ville": "Stoneham-et-Tewkesbury ",
+   "pop": 9134,
+   "latitude": 46.99917,
+   "longitude": -71.36806,
+   "codeMRC": 22,
+   "MRC": "La Jacques-Cartier"
+ },
+ {
+   "code": 13073,
+   "ville": "Témiscouata-sur-le-Lac ",
+   "pop": 4913,
+   "latitude": 47.68333,
+   "longitude": -68.88333,
+   "codeMRC": 13,
+   "MRC": "Témiscouata"
+ },
+ {
+   "code": 64008,
+   "ville": "Terrebonne ",
+   "pop": 117615,
+   "latitude": 45.69167,
+   "longitude": -73.63444,
+   "codeMRC": 64,
+   "MRC": "Les Moulins"
+ },
+ {
+   "code": 31084,
+   "ville": "Thetford Mines ",
+   "pop": 25618,
+   "latitude": 46.08889,
+   "longitude": -71.30194,
+   "codeMRC": 31,
+   "MRC": "Les Appalaches"
+ },
+ {
+   "code": 11040,
+   "ville": "Trois-Pistoles ",
+   "pop": 3152,
+   "latitude": 48.12556,
+   "longitude": -69.17528,
+   "codeMRC": 11,
+   "MRC": "Les Basques"
+ },
+ {
+   "code": 37067,
+   "ville": "Trois-Rivières ",
+   "pop": 138200,
+   "latitude": 46.34306,
+   "longitude": -72.54333,
+   "codeMRC": 371,
+   "MRC": "Trois-Rivières"
+ },
+ {
+   "code": 89008,
+   "ville": "Val-d'Or ",
+   "pop": 32896,
+   "latitude": 48.10194,
+   "longitude": -77.78944,
+   "codeMRC": 89,
+   "MRC": "La Vallée-de-l'Or"
+ },
+ {
+   "code": 82015,
+   "ville": "Val-des-Monts ",
+   "pop": 12241,
+   "latitude": 45.665,
+   "longitude": -75.73417,
+   "codeMRC": 82,
+   "MRC": "Les Collines-de-l'Outaouais"
+ },
+ {
+   "code": 40043,
+   "ville": "Val-des-Sources ",
+   "pop": 6840,
+   "latitude": 45.7775,
+   "longitude": -71.93833,
+   "codeMRC": 40,
+   "MRC": "Les Sources"
+ },
+ {
+   "code": 71083,
+   "ville": "Vaudreuil-Dorion ",
+   "pop": 41017,
+   "latitude": 45.40361,
+   "longitude": -74.03667,
+   "codeMRC": 71,
+   "MRC": "Vaudreuil-Soulanges"
+ },
+ {
+   "code": 39062,
+   "ville": "Victoriaville ",
+   "pop": 47466,
+   "latitude": 46.05583,
+   "longitude": -71.96056,
+   "codeMRC": 39,
+   "MRC": "Arthabaska"
+ },
+ {
+   "code": 85025,
+   "ville": "Ville-Marie ",
+   "pop": 2476,
+   "latitude": 47.32889,
+   "longitude": -79.4375,
+   "codeMRC": 85,
+   "MRC": "Témiscamingue"
+ },
+ {
+   "code": 42088,
+   "ville": "Windsor ",
+   "pop": 5327,
+   "latitude": 45.56861,
+   "longitude": -72.00583,
+   "codeMRC": 42,
+   "MRC": "Le Val-Saint-François"
+ }
+]
+
+montreal =  {
+   "code": 66023,
+   "ville": "Montréal ",
+   "pop": 1806022,
+   "latitude": 45.50889,
+   "longitude": -73.55417,
+   "codeMRC": 663,
+   "MRC": "Communauté métropolitaine de Montréal"
+ }
