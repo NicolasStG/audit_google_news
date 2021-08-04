186551913e624e9cc82e23b07ef2ed2cd20bb53c
Nicolas St-Germain <niikostg@gmail.com>
2021-08-04 11:12:58 -0400

    fixed a few exceptions bugg related to three formula

--- script_run/Montreal_Local_V2.py
+++ script_run/Montreal_Local_V2.py
@@ -4,6 +4,7 @@
 # ©2020 Nicolas St-Germain GNU GPL v3.
 
 import csv, errno, os, os.path, re, requests, time
+from logging import exception
 
 from datetime import datetime
 
@@ -136,16 +137,21 @@ def get_localisation_confirmation(infos : str, villes : str) -> filepost:
                 browser.close()
                 
                 successful = True  # If this is reached, then my_stuff() worked
-            except WebDriverException:
+            except Exception as i:
+                print(i)
                 browser.close()
                 time.sleep(15)
 
 
 def get_article_url(article : WebElement) -> str:
     #URL ---> DÉTERMINER COMMENT DIFFÉRENCIER LOCAL, NATIONAL, RÉGIONAL ET HORS-CANADA
-    lien_article = article.find_element_by_class_name("VDXfz").get_attribute('href')
-    r = requests.get(lien_article, allow_redirects=False)
-    return r.headers['Location']
+    try :
+        lien_article = article.find_element_by_class_name("VDXfz").get_attribute('href')
+        r = requests.get(lien_article, allow_redirects=False)
+        return r.headers['Location']
+    
+    except KeyError:
+        return "" 
 
 
 
@@ -230,7 +236,8 @@ def research_term_in_city(villes, creation_fichier, term, infos) -> None:
 
             
             successful = True  # If this is reached, then my_stuff() worked
-        except TimeoutException :
+        except Exception as i:
+            print(i)
             browser.close()
             time.sleep(15)
 
