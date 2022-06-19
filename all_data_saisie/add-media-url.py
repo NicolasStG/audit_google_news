77086fcd97062e583ed125c7aae2e7ac3aeb206e
NicolasStG <niikostg@gmail.com>
2022-06-19 17:42:56 -0400

    big bug fix where MTL_V2 were not in database

--- all_data_saisie/add-media-url.py
+++ all_data_saisie/add-media-url.py
@@ -1,11 +1,11 @@
 import csv
 
 def get_file_name():
-    return "./V2_full_data.csv"
+    return "/Users/nicolasst-germain/Documents/GitHub/audit_google_news/all_data_saisie/all_data/fulldata_without_mediatype.csv"
 
 def main() -> None:
     reader = csv.reader(open(get_file_name(), 'r'))
-    writer = csv.writer(open('output_full_data.csv', 'w'))
+    writer = csv.writer(open('output_full_data_without_mediatype.csv', 'w'))
     next(reader)
     writer.writerow(["Villes", "Mots-clés", "position du média", "Nom du média", "Titre de l'article", "Date de publication", "Journée de différence","URL", "date et heure saisie", "type mot-clés", "media_url"])
     for row in reader:
